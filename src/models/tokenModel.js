87cba9f1f67c59c75892218e81193b9517519cd5
foley61 <coder61616@gmail.com>
2024-06-11 15:01:45 +0300

    up

---
+++ src/models/tokenModel.js
@@ -0,0 +1,32 @@
+/* -------------------------------------------------------
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
+------------------------------------------------------- */
+const mongoose = require("mongoose")
+/* ------------------------------------------------------- */
+// Token Model:
+
+const TokenSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true,
+        index: true,
+    },
+
+    token: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true
+    }
+
+}, {
+    collection: 'tokens',
+    timestamps: true
+})
+
+// Model:
+module.exports = mongoose.model('Token', TokenSchema)
---
+++ src/models/userModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+},{
+    collection: "Users",
+    timestamps: true
+})
+module.exports = mongoose.model("Users",userSchema)
